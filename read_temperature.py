7f16f83e8325fbd1afd3181fed6f20ba10440df9
Sergio Solinas <theiera@gmail.com>
2016-05-10 20:36:08 +0200

    Update read_temperature.py
    Added plot commands.

--- read_temperature.py
+++ read_temperature.py
@@ -1,3 +1,4 @@
+
 from datetime import datetime
 import numpy as np
 
@@ -20,7 +21,22 @@ d_t_temp =  np.genfromtxt(f, delimiter=" ", converters={0: conv_date,1: conv_tim
 # print d_t_temp
 f.close()
 data_temp = []
-for f in d_t_temp:
+to_plot = np.zeros( (len(d_t_temp),2) )
+print to_plot.shape
+for i,f in enumerate(d_t_temp):
     data_temp.append([datetime.combine(datetime.date(f[0]), datetime.time(f[1])), f[2]])
+    to_plot[i,0] =  datetime.strftime(data_temp[-1][0],'%f')
+    to_plot[i,1] =  data_temp[-1][1]
 
-#print data_temp
+
+to_plot = np.array(to_plot)
+print datetime.strftime(data_temp[0][0],'%f')
+
+# Grafici
+from matplotlib import pylab
+
+f = pylab.figure()
+pylab.plot(to_plot[:,0], to_plot[:,1])
+pylab.xlabel('Tempo in ms dal 1 Gen 1900')
+pylab.ylabel('Temperatura (C)')
+pylab.show()
