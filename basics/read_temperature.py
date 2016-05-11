46aab35dda8a4f9c4aef29ad373f5c7c84818f7f
Raspberry PI <pi@rspberry.com>
2016-05-11 16:58:27 +0200

    Aggiornato file di generazione grafici.

--- basics/read_temperature.py
+++ basics/read_temperature.py
@@ -1,12 +1,13 @@
 
 from datetime import datetime
 import numpy as np
+import time
 
 data = datetime.strptime("07-05-2016","%d-%m-%Y")
 
 tempo = datetime.strptime('09:13:37','%H:%M:%S')
 
-conv_date = lambda x: datetime.strptime(x,"%d-%m-%Y")
+conv_date = lambda x: datetime.strptime(x,"%Y-%m-%d")
 conv_temp = lambda x: x 
 def conv_time(x):
     if '.' in x:
@@ -16,7 +17,7 @@ def conv_time(x):
         t = datetime.strptime(x,'%H:%M:%S')
     return t
         
-f = open('Temp_dati.dat')
+f = open('dati11maggio.txt')
 d_t_temp =  np.genfromtxt(f, delimiter=" ", converters={0: conv_date,1: conv_time,2: conv_temp})
 # print d_t_temp
 f.close()
@@ -25,7 +26,8 @@ to_plot = np.zeros( (len(d_t_temp),2) )
 print to_plot.shape
 for i,f in enumerate(d_t_temp):
     data_temp.append([datetime.combine(datetime.date(f[0]), datetime.time(f[1])), f[2]])
-    to_plot[i,0] =  datetime.strftime(data_temp[-1][0],'%f')
+    #to_plot[i,0] =  datetime.strftime(data_temp[-1][0],'%f')
+    to_plot[i,0] =  time.mktime(data_temp[-1][0].timetuple())
     to_plot[i,1] =  data_temp[-1][1]
 
 
@@ -37,6 +39,7 @@ from matplotlib import pylab
 
 f = pylab.figure()
 pylab.plot(to_plot[:,0], to_plot[:,1])
+#pylab.plot( to_plot[:,0])
 pylab.xlabel('Tempo in ms dal 1 Gen 1900')
 pylab.ylabel('Temperatura (C)')
  
