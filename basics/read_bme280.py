b221e8c30f77c42a5dcb14c64464a3ece9bf28ce
Raspberry PI <pi@raspberry.com>
2016-05-18 11:25:40 +0200

    Aggiunto software per leggere dati dal BME280 Weather Click board.

---
+++ basics/read_bme280.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python
+
+# Read the BME280 sensor from the Weather Click board
+# Requirements:
+# sudo apt-get python i2c-tools, python-smbus
+# sudo pip install bme280
+
+# -*- coding: utf-8 -*-
+import re
+import sys,time
+
+from bme280.bme280 import bme_read
+from datetime import datetime
+
+
+if __name__ == '__main__':
+    while True:
+        data = bme_read() 
+        now = datetime.now().strftime("%Y-%m-%d %H:%M:%S") 
+        to_write = "%s %7.2f KPa %07.2f %% %07.2f C\n" % (now,data.pressure,data.humidity, data.temperature)
+	f = open('data.log',"a")
+	f.write(to_write)
+	f.close()
+        time.sleep(1)
+    # sys.argv[0] = re.sub(r'(-script\.pyw|\.exe)?$', '', sys.argv[0])
+    # sys.exit(main())
