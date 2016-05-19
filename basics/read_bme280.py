bdd93c596df77ed4e0cd4033e1532de7315e86e8
Raspberry Pi <pi@raspberry.com>
2016-05-19 17:44:05 +0200

    Small changes to bme280 reader.

--- basics/read_bme280.py
+++ basics/read_bme280.py
@@ -18,6 +18,7 @@ if __name__ == '__main__':
         data = bme_read() 
         now = datetime.now().strftime("%Y-%m-%d %H:%M:%S") 
         to_write = "%s %7.2f KPa %07.2f %% %07.2f C\n" % (now,data.pressure,data.humidity, data.temperature)
+	print to_write
         f = open('data.log',"a")
         f.write(to_write)
         f.close()
